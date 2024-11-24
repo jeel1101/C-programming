0dd899ef7bef940fe085eef0cfaa393e41f07a2d
Patel Jeel <s22cpjeel107@gmail.com>
2024-11-24 12:31:55 +0530

    Pattern 30

---
+++ Patterns/Pattern 30.c
@@ -0,0 +1,22 @@
+//Inveted right angled pyramid
+
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    
+    for(i=n;i>=1;i--){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
