d668c4e980583e5709caa4b4710c1146e2b485be
Patel Jeel <s22cpjeel107@gmail.com>
2024-11-24 13:22:51 +0530

    Pattern 37

---
+++ Patterns/Pattern 37.c
@@ -0,0 +1,20 @@
+//Floyd's triangle
+
+#include<stdio.h>
+int main()
+{
+    int i,j,n,num=1;
+    
+    printf("Enter the rows: ");
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%d ",num);
+            num++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
