d9bf28399c0ac7bda978a237cb3141813809f0a5
Patel Jeel <s22cpjeel107@gmail.com>
2024-11-24 13:07:57 +0530

    Pattern 34

---
+++ Patterns/Pattern 34.c
@@ -0,0 +1,25 @@
+//diamond pyramid
+
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("\t");
+            if(i==1 || i==n || j==1 || j==n){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
