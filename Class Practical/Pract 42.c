b967e6a07e90f3cf43ec8a2d5e338d5720b3ef88
Patel Jeel <s22cpjeel107@gmail.com>
2024-11-30 21:22:20 +0530

    Pract 42

---
+++ Class Practical/Pract 42.c
@@ -0,0 +1,34 @@
+//decending order
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  
+    
+    printf("Enter the numbers:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("Descending order is:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
