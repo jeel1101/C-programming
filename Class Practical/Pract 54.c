e77712a6b5f3d9c5636acf99c45e0b5f74fd433b
Patel Jeel <s22cpjeel107@gmail.com>
2024-12-01 15:40:53 +0530

    Pract 54

---
+++ Class Practical/Pract 54.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\nBefore swapping: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("\nAfter swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
