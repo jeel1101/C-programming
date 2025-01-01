bc99c9d1ab60e56acf318327d1d92ec6dde028c5
Patel Jeel <s22cpjeel107@gmail.com>
2025-01-01 10:08:56 +0530

    calloc( ).c

---
+++ #Sem 2/calloc( ).c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int* ptr;
+    int n;
+    
+    printf("Enter the size of array you want to create: ");
+    scanf("%d",&n);
+    
+    ptr= (int*)calloc(n,sizeof(int));
+    
+    for(int i=0;i<n;i++){
+        printf("Enter %d element: \n",i);
+        scanf("%d",&ptr[i]);
+    }
+    
+    printf("\n\n");
+    for(int i=0;i<n;i++){
+        printf("Element at index %d is %d\n",i,ptr[i]);
+    }
+    
+    return 0;
+}
