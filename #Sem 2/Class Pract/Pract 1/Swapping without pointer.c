f7cfa55bdc6ea9355a4c5d4f6f3cf190d3f2f76a
Patel Jeel <s22cpjeel107@gmail.com>
2025-01-24 15:38:17 +0530

    Swapping without pointer.c

---
+++ #Sem 2/Class Pract/Pract 1/Swapping without pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+    
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+   
+    temp = a;
+    a = b;
+    b = temp;
+    
+    
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    
+    return 0;
+}
